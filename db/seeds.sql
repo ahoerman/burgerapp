edd05566a2a20bdb2c6cae0730305796c9742e65
A Hoerman <awhoerman@me.com>
2020-03-29 22:04:43 -0400

    added file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('All American');
+INSERT INTO burgers (name) VALUES ('Baconater');
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Portabello Burger', true);
+INSERT INTO burgers (name) VALUES ('Sushi Burger');
