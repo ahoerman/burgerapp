efdc0fec816f99466448cd75eb70014b05fea04c
A Hoerman <awhoerman@me.com>
2020-03-30 01:37:38 -0400

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -9,16 +9,16 @@ var connection = mysql.createConnection({
   database: "burgerdb"
 });
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root1234',
-//     database: 'burgersdb'
-//   });
-// };
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root1234',
+    database: 'burgersdb'
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
