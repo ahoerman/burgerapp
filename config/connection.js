96f3ad056605dc035c3a746ba868a828b5821174
A Hoerman <awhoerman@me.com>
2020-03-29 22:43:33 -0400

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "burgerdb"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root1234',
+    database: 'burgersdb'
+  });
+};
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
